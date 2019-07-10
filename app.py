542db13db5904df6e28ce440eb314e32fa297061
andrew.maurer <maurer.ca@gmail.com>
2019-07-10 16:15:40 -0700

    Initial commit

---
+++ app.py
@@ -0,0 +1,116 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Jul 10 2019
+
+@author: Andrew Maurer
+"""
+
+import os
+import pathlib as pl
+import dash
+import dash_table
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly.graph_objs as go
+import pandas as pd
+from dash.dependencies import Input, Output, State
+from myfunctions import sp500_estimate
+from myfunctions import dash_regtable
+
+table_header_style = {
+    "backgroundColor": "rgb(2,21,70)",
+    "color": "white",
+    "textAlign": "center",
+}
+
+app = dash.Dash(__name__)
+
+server = app.server
+
+APP_PATH = str(pl.Path(__file__).parent.resolve())
+
+pkdata = pd.read_csv(os.path.join(APP_PATH, os.path.join("data", "pkdata.csv")))
+
+##### Start #########
+# First time series
+Tk = 'BAC'
+df = pd.read_pickle('Data/sp500_6stocks.pkl')
+dfw = df.pivot(index='Date', columns='Ticker', values='Close') #[['SPY',Tk]]
+dfw.index = pd.to_datetime(dfw.index)
+dfw.dropna(inplace=True)
+for col in dfw.columns:
+    dfw[col]=dfw[col]/dfw[col].dropna()[0]*100
+
+# Prepare data for modeling
+dfc = df.copy()
+start = min(dfc[dfc.Ticker=='SPY']['Date'])
+dfc["Return"] = dfc.groupby("Ticker")["Close"].pct_change(1)
+dfc = dfc[dfc['Date']>=start]
+# print(([sp500_estimate(df,Tk).summary()]))
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+app.layout = html.Div([
+    html.Div([dcc.Dropdown(id="selected-value", multi=True, value=["SPY"],
+                        options=[{"label":x,"value":x} for x in dfw])],
+          className="row", style={"display": "block", "width": "30%", "margin-left": "auto",
+                                  "margin-right": "auto"}),
+    dcc.Graph(id='graph-with-slider'),
+    dcc.Slider(
+        id='year-slider',
+        min=2000,
+        max=2010,
+        value=2000,
+        marks={str(2003): str(2003)},
+        step=None
+    ),
+    html.Div(id='regtable-div')
+])
+
+
+@app.callback(
+    Output('graph-with-slider', 'figure'),
+    [Input('selected-value', 'value'),Input('year-slider', 'value')])
+def update_figure(selected,selected_year):
+    traces = []
+    for i in selected:
+        print(i)
+        traces.append(go.Scatter(
+            x=dfw.index,
+            y=dfw[i],
+            text=i,
+            mode='lines',
+            name=i
+        ))
+
+    return {
+        'data': traces,
+        'layout': go.Layout(
+            xaxis={'title': 'Date'},
+            yaxis={'title': 'Normalized value'} #,
+            # margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
+            # legend={'x': 0, 'y': 1},
+            # hovermode='closest'
+        )
+    }
+
+@app.callback(
+    Output(component_id='regtable-div', component_property='children'),
+    [Input('selected-value', 'value')]
+)
+def update_output_div(tickers):
+    Tk = tickers[-1]
+    if len(tickers)==1:
+        return ''
+    model1 = sp500_estimate(dfc, Tk) #.summary() #.as_html()
+    allmodels = [sp500_estimate(dfc, x) for x in tickers[1:]]
+    #return 'You entered ' + Tk + ' single. All: ' + ' '.join(tickers) + '"{}"'.format(4)
+    #return [dash_regtable([model1]),dash_regtable([model1])]
+    return html.Div(className='row', children=[
+        dash_regtable([m]) for m in allmodels
+    ])
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
---
+++ myfunctions.py
@@ -0,0 +1,87 @@
+import pandas as pd
+import numpy as np
+import sys
+import statsmodels.api as sm
+import os
+import dash_html_components as html
+os.chdir('C:/Users/andrew.maurer/PycharmProjects/StressTestingWarmup')
+
+# Load and process data
+# Tk = 'BAC'
+# df = pd.read_pickle('Data/sp500_6stocks.pkl')
+# start = min(df[df.Ticker=='SPY']['Date'])
+# df["Return"] = df.groupby("Ticker")["Close"].pct_change(1)
+# df = df[df['Date']>=start]
+
+def sp500_estimate(df,Tk):
+    # Reshape data
+    dfw = df.pivot(index='Date', columns='Ticker', values='Close')[['SPY',Tk]]
+    dfr = df.pivot(index='Date', columns='Ticker', values='Return')[['SPY',Tk]] # Reshape long to wide
+    dfr.index = pd.to_datetime(dfr.index)
+    dfr = dfr.dropna()
+    X = pd.DataFrame(dfr['SPY'].dropna())
+    Y = dfr[[Tk]].dropna()
+    Y = pd.Series(dfr[Tk].dropna())
+    X.insert(0, 'Intercept', 1)
+
+    # Different models
+    model1 = sm.OLS(Y, X)
+    results1 = model1.fit()
+
+    # Trailing x-day return
+    dflagreturn = pd.concat([dfw[Tk].pct_change(1).shift(),dfw[Tk].pct_change(5).shift(),dfw[Tk].pct_change(21).shift()],axis=1)
+    dflagreturn.columns = ['Self_Trail1','Self_Trail5','Self_Trail21']
+    dflagreturn.index = pd.to_datetime(dflagreturn.index)
+
+    # Concatenate X variables
+    X = pd.concat([X,dflagreturn],axis=1).dropna()
+
+    # Intersect X and Y
+    intersect = Y.index.intersection(X.index)
+    Y = Y[intersect]
+    X = X.loc[intersect]
+
+    # Model 1 - Intercept/SPY
+    vars = ['Intercept','SPY']
+    model1 = sm.OLS(Y, X[vars])
+    results1 = model1.fit()
+    return results1
+
+# Run model eg
+# print(([sp500_estimate(df,Tk).summary()]))
+
+def dash_regtable(modellist):
+    N = len(modellist)
+    betas = pd.concat([r.params for r in modellist], axis=1, sort=False)
+    pvalues = pd.concat([r.pvalues for r in modellist], axis=1, sort=False)
+    se = pd.concat([r.bse for r in modellist], axis=1, sort=False)
+    params = betas.index
+
+    # Header
+    S = []
+    # Column titles
+    row = [modellist[0].model.endog_names]+['('+str(i+1)+')' for i in range(N)]
+    S = S+[html.Tr([html.Th(x) for x in row])]
+
+    # Variables
+    for p in params:
+        row = [p]
+        for i,pval in zip(betas.loc[p],pvalues.loc[p]):
+            stars = '***' if pval < 0.01 else ('**' if pval < 0.05 else ('*' if pval < 0.1 else ''))
+            row = row+['' if np.isnan(i) else '{0:.5f}'.format(i)+stars]
+        S = S + [html.Tr([html.Td(x) for x in row])]
+        row = [' ']+[('' if np.isnan(x) else '({0:.5f}'.format(x)+')') for x in se.loc[p]]
+        S = S + [html.Tr([html.Td(x) for x in row])]
+
+    # Statistics
+    stat_labels = ['Observation','Parameters','R-squared','Adj. R-squared']
+    stat_formats = ['{:.0f}','{:.0f}','{0:.5f}','{0:.5f}']
+    d = {}
+    for i,m in enumerate(modellist):
+        for j,s in enumerate([m.nobs,m.df_model+1,m.rsquared,m.rsquared_adj]):
+          d[i,j] = s
+    for (j,lab),fmt in zip(enumerate(stat_labels),stat_formats):
+        row = [lab]+[fmt.format(d[i,j]) for i,m in enumerate(modellist)]
+        S = S + [html.Tr([html.Td(x) for x in row])]
+
+    return html.Table(S, className='three columns')
